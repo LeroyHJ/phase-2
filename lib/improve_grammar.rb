95d7e7d9aba726a44434cab8d9d7f927b8cadfb7
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2022-08-18 19:32:48 +0100

    improve grammar

--- lib/improve_grammar.rb
+++ lib/improve_grammar.rb
@@ -1,2 +1,4 @@
-def improve_grammar()
+def improve_grammar(text)
+  arr = text.split("")
+  ("A".."Z").include?(arr[0]) && (".""!""?").include?(arr[-1])
 end
--- spec/improve_grammar_spec.rb
+++ spec/improve_grammar_spec.rb
@@ -1,7 +1,19 @@
-require "improve_grammar"
+require 'improve_grammar'
 
 RSpec.describe "improve_grammar method" do
   it "returns true" do
     expect(improve_grammar("Hello, my name is Yichao.")).to eq true
+  end
+
+  it "returns false" do
+    expect(improve_grammar("hello, my name is Yichao.")).to eq false
+  end
+
+  it "returns false" do 
+    expect(improve_grammar("Hello, my name is Yichao")).to eq false
+  end
+
+  it "returns true" do 
+    expect(improve_grammar("Hello, my name is Yichao!")).to eq true
   end
 end
